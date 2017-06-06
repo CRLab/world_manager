2dc8d581746156958d6d7cf873ff6632e22ecc53
Jake Varley <jakevarley@gmail.com>
2017-06-06 12:49:02 -0400

    initial commit

--- scripts/world_manager_node.py
+++ scripts/world_manager_node.py
@@ -19,99 +19,49 @@ import moveit_msgs.srv
 from graspit_msgs.srv import *
 import graspit_msgs.msg
 
-
 from moveit_commander import PlanningSceneInterface
-from model_rec_manager import ModelManager, ModelRecManager
-from object_filename_dict import file_name_dict
-
-
-
-
-#these imports are specifically for interactive markers
-import rospy
-import copy
-from math import sin, cos
-
-
-from geometry_msgs.msg import Point
-from tf.broadcaster import TransformBroadcaster
-
-from random import random
-from math import sin
+from model_manager import ModelPoseBroadcaster
 
 
 class WorldManager:
-    def __init__(self, server):
 
-        self.server = server
+    def __init__(self):
+
         moveit_commander.roscpp_initialize(sys.argv)
 
         self.planning_scene_topic = rospy.get_param("planning_scene_topic")
-        self.run_recognition_topic = rospy.get_param("run_recognition_topic")
-        self.detected_model_frame_id=rospy.get_param("detected_model_frame_id")
 
         self.scene = PlanningSceneInterface()
-        self.robot = moveit_commander.RobotCommander()
 
-        self.model_manager = ModelRecManager()
+        self.model_pose_broadcaster = ModelPoseBroadcaster()
 
-        self.planning_scene_service_proxy = rospy.ServiceProxy(self.planning_scene_topic, moveit_msgs.srv.GetPlanningScene)
+        self.clear_planning_scene_service = rospy.Service("/world_manager/clear_planning_scene", graspit_msgs.srv.ClearModels, self.remove_all_objects_from_planner)
+        self.add_mesh_to_planning_scene_service = rospy.Service("/world_manager/add_object", graspit_msgs.srv.AddObject, self.add_object_to_planning_scene)
 
-        self._run_recognition_as = actionlib.SimpleActionServer(self.run_recognition_topic,
-                                                                graspit_msgs.msg.RunObjectRecognitionAction,
-                                                                execute_cb=self._run_recognition_as_cb,
-                                                                auto_start=False)
-        self._run_recognition_as.start()
+        rospy.sleep(1.0)
+        self.add_walls()
         rospy.loginfo("World Manager Node is Up and Running")
 
-    def _run_recognition_as_cb(self, goal):
-        print("_run_recognition_as_cb")
+    def add_object_to_planning_scene(self, request):
+        self.remove_all_objects_from_planner(None)
+        #this makes sure tf is continually broadcast
+        self.model_pose_broadcaster.add_model(request.objectname, request.pose_stamped)
 
-        print("about to remove_all_objects_from_planner()")
-        self.remove_all_objects_from_planner()
-        print("finished remove_all_objects_from_planner()")
+        #remove the old completion if it is there
+        self.scene.remove_world_object(request.objectname)
 
-        self.model_manager.refresh()
-
-        print("about to add_all_objects_to_planner()")
-        self.add_all_objects_to_planner()
-        print("finished add_all_objects_to_planner()")
-
-        _result = graspit_msgs.msg.RunObjectRecognitionResult()
-        print("graspit_msgs.msg.RunObjectRecognitionResult()")
-
-        for model in self.model_manager.model_list:
-            object_info = graspit_msgs.msg.ObjectInfo(model.object_name, model.model_name, model.get_world_pose())
-            _result.object_info.append(object_info)
-            position = model.get_world_pose().position
-            position = Point(position.x, position.y, position.z)
-            make6DofMarker(fixed=False, 
-                frame=model.model_name,
-                interaction_mode=InteractiveMarkerControl.MOVE_ROTATE_3D,
-                position=position, 
-                show_6dof = True)
-        
-            
-        print("finished for loop")
-        server.applyChanges()
-        
-
-        self._run_recognition_as.set_succeeded(_result)
+        #add the new object to the planning scene
+        self.scene.add_mesh(request.objectname, request.pose_stamped, request.mesh_filepath)
         return []
 
     def get_body_names_from_planner(self):
         rospy.wait_for_service(self.planning_scene_topic, 5)
-        components = PlanningSceneComponents(
-            PlanningSceneComponents.WORLD_OBJECT_NAMES + PlanningSceneComponents.TRANSFORMS)
 
-        ps_request = moveit_msgs.srv.GetPlanningSceneRequest(components=components)
-        ps_response = self.planning_scene_service_proxy.call(ps_request)
-
-        body_names = [co.id for co in ps_response.scene.world.collision_objects]
+        body_names = self.scene.get_known_object_names()
 
         return body_names
 
-    def remove_all_objects_from_planner(self):
+    def remove_all_objects_from_planner(self, request):
 
         body_names = self.get_body_names_from_planner()
 
@@ -122,29 +72,16 @@ class WorldManager:
 
             body_names = self.get_body_names_from_planner()
 
-    def add_all_objects_to_planner(self):
-        self.add_obstacles()
-        for model in self.model_manager.model_list:
-            model_name = model.model_name.strip('/')
-            print "Adding " + str(model_name) + "To Moveit"
-            filename = file_name_dict[model_name]
-            if os.path.isfile(filename):
+        self.model_pose_broadcaster.clear_models()
 
-                stamped_model_pose = geometry_msgs.msg.PoseStamped()
-                stamped_model_pose.header.frame_id = self.detected_model_frame_id
-                stamped_model_pose.pose = model.get_world_pose()
-
-                self.scene.add_mesh(model.object_name, stamped_model_pose, filename)
-
-            else:
-                rospy.logwarn('File doesn\'t exist - object %s, filename %s' % (model.object_name, filename))
+        self.add_walls()
+        return []
 
     def add_walls(self):
         walls = rospy.get_param('/walls')
         for wall_params in walls:
             rospy.loginfo("Adding wall " + str(wall_params))
             self.add_wall(wall_params)
-        return
 
     def add_wall(self, wall_params):
         name = wall_params["name"]
@@ -162,11 +99,7 @@ class WorldManager:
 
         back_wall_pose = geometry_msgs.msg.PoseStamped()
         back_wall_pose.header.frame_id = '/' + frame_id
-        wall_dimensions = [
-            x_thickness, 
-            y_thickness,  
-            z_thickness
-        ]
+        wall_dimensions = [x_thickness, y_thickness,  z_thickness]
 
         back_wall_pose.pose.position = geometry_msgs.msg.Point(**{'x': x, 'y': y, 'z': z})
         back_wall_pose.pose.orientation = geometry_msgs.msg.Quaternion(**{'x': qx,
@@ -175,22 +108,21 @@ class WorldManager:
                                                                           'w': qw})
         self.scene.add_box(name, back_wall_pose, wall_dimensions)
 
-        return
-
-    def add_obstacles(self):
-        self.add_walls()
 
 if __name__ == '__main__':
 
     try:
+        
         rospy.init_node('world_manager_node')
 
-        world_manager = WorldManager(server)
-        world_manager.add_obstacles()
-        loop = rospy.Rate(30)
+        rospy.loginfo("Starting Up World Manager")
+        world_manager = WorldManager()
+        
+        rospy.loginfo("World Manager Has Started")
 
+        loop = rospy.Rate(30)
         while not rospy.is_shutdown():
-            world_manager.model_manager.rebroadcast_object_tfs()
+            world_manager.model_pose_broadcaster.broadcast_object_tfs()
             loop.sleep()
 
     except rospy.ROSInterruptException:
