a395d016df8e96f530e924e522dda620be8764d5
Jake Varley <jakevarley@gmail.com>
2017-10-18 12:18:12 -0400

    renamed message type in client, reformated

--- src/world_manager/world_manager_client.py
+++ src/world_manager/world_manager_client.py
@@ -2,27 +2,24 @@ import world_manager.srv
 import std_srvs.srv
 import rospy
 
-class WorldManagerClient():
 
-    def add_mesh(self,
-                 mesh_name,
-                 mesh_filepath,
-                 pose_stamped):
+class WorldManagerClient():
+    def add_mesh(self, mesh_name, mesh_filepath, pose_stamped):
 
         service_proxy = rospy.ServiceProxy("/world_manager/add_mesh",
-                                                  world_manager.srv.AddObject)
+                                           world_manager.srv.AddMesh)
         service_proxy.wait_for_service(timout=5)
         service_proxy(mesh_name, mesh_filepath, pose_stamped)
 
     def clear_objects(self):
         service_proxy = rospy.ServiceProxy("/world_manager/clear_objects",
-                                                  std_srvs.srv.Empty)
+                                           std_srvs.srv.Empty)
         service_proxy.wait_for_service(timeout=5)
         service_proxy()
 
     def add_walls(self):
         service_proxy = rospy.ServiceProxy("/world_manager/add_walls",
-                                                  std_srvs.srv.Empty)
+                                           std_srvs.srv.Empty)
         service_proxy.wait_for_service(timeout=5)
         service_proxy()
 
@@ -31,6 +28,3 @@ class WorldManagerClient():
                                            world_manager.srv.AddTF)
         service_proxy.wait_for_service(timeout=5)
         service_proxy(frame_name, pose_stamped)
-
-
-
